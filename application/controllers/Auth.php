a08dfdfd23169e423f7872d48dfa223630264498
iqbaladhis <iqbaladhis@gmail.com>
2021-01-01 13:19:08 +0700

    .

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -70,13 +70,57 @@ class Auth extends CI_Controller
         ];
 
         $this->load->library('email', $config);
+        $this->email->initialize($config);
+        // $filename = 'assets/images/emaillogo.png';
+        // $this->email->attach($filename);
 
-        $this->email->from('system@blitzindoutama.com', 'Blitzindo Utama');
+        $this->email->from('system@blitzindoutama.com', 'Blitzindo');
         $this->email->to($this->input->post('email'));
 
         if ($type == 'forgot') {
-            $this->email->subject('Reset Password');
-            $this->email->message('Click this link to reset your password : <a href="' . base_url() . 'auth/resetpassword?email=' . $this->input->post('email') . '&token=' . urlencode($token) . '">Reset Password</a>');
+            $this->email->subject('[Blitzindo Utama] Password Reset Request for Blitzindo Utama');
+            // $cid = $this->email->attachment_cid($filename);
+            // $this->email->message('<img src="cid:' . $cid . '" alt="logoblitz"');
+            $this->email->message('We heard that you lost your password. Sorry about that! <br><br>  But don’t worry! You can use the following link to reset your password: <br> <a href="' . base_url() . 'auth/resetpassword?email=' . $this->input->post('email') . '&token=' . urlencode($token) . '">Reset Password</a> <br><br> Best Regrads, <br> Blitzindo Utama Team <br><br><br>   <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+            <HTML><HEAD><TITLE>Email Signature</TITLE>
+            <META content="text/html; charset=utf-8" http-equiv="Content-Type">
+            </HEAD>
+            <BODY style="font-size: 10pt; font-family: Arial, sans-serif;">
+            
+            <table style="width: 515px; font-size: 10pt; font-family: Arial, sans-serif;"  cellspacing="0" cellpadding="0">
+              <tbody>
+              <tr>
+                <td style="font-size: 14pt;  color:#000e58; font-family: Arial, sans-serif; width: 514px; padding-bottom: 10px; vertical-align:top;" colspan="2" valign="top">  <strong><span style="font-size: 14pt; font-family: Arial, sans-serif; color:#fcb01c;">Blitzindo Utama</span></strong><br>  <span style="font-size: 10pt; font-family: Arial, sans-serif; color:#000000;">Handling expatriate legality in Indonesia.</span></td>
+              </tr>
+              <tr><td style="font-size: 10pt; border-top: 1px solid; border-top-color: #fcb01c; color:#363636; font-family: Arial, sans-serif;  padding-top: 10px; vertical-align:top;" colspan="2" valign="top">
+                 <span style="font-size: 10pt; font-family: Arial, sans-serif; color:#363636"><strong>PT. Blitzindo Utama</strong><br></span> 
+                 <span style="font-size: 10pt; font-family: Arial, sans-serif; color:#363636">Mega Glodok Kemayoran Blok E 18 Jl. Angkasa Kav. B6, Kemayoran, Jakarta Pusat<br></span>
+                 <span style="font-size: 10pt; font-family: Arial, sans-serif; color:#363636">10616, Indonesia<br></span>
+                 
+                 
+                 <span style="font-size: 10pt; font-family: Arial, sans-serif; color:#363636;">email: system@blitzindoutama.com<br></span>								</td>
+              </tr>  
+              <tr>
+                <td style="font-size: 10pt; padding-bottom: 10px; padding-top: 10px; vertical-align:top;"  valign="top"><strong>	<a href="http://www.blitzindoutama.com" target="_blank" rel="noopener" style="text-decoration:none;"><span style="color:#000e58; font-family: Arial, sans-serif;"><span style="font-size: 10pt; font-family: Arial, sans-serif; color:#fcb01c;">www.blitzindoutama.com</span></span></a></strong></td>
+                <td style="font-size: 10pt; font-family: Arial, sans-serif; padding-bottom: 10px; padding-top: 10px; vertical-align:top; text-align:right;"  valign="top" align="right">
+                 </td>	 	 	
+              </tr>
+              <tr>
+                <td style="font-size: 10pt; font-family: Arial, sans-serif; padding-bottom: 10px; padding-top: 10px; vertical-align:top;" colspan="2" valign="top">	
+                 <a href="https://www.codetwo.com/email-signatures/" target="_blank" rel="noopener">
+                  <img alt="Banner" style="width:514px; height:auto; border:0;" src="https://drive.google.com/uc?export=download&amp;id=1z_GSL9LwkbG3mRj8rZO0EeKtnTDjyKvS" width="514" border="0">
+                 </a>	 </td>
+              </tr>
+              <tr>
+                <td style="font-size: 9pt; color:#363636; font-family: Arial, sans-serif; padding-top: 10px; vertical-align:top;" colspan="2"  valign="top">      
+                 <div style="color:#363636; font-size: 9pt; font-family: Arial, sans-serif; text-align:justify; width:514px;"  align="justify">The content of this message is confidential. If you have received it by mistake, please inform us by an email reply and then delete the message. It is forbidden to copy, forward, or in any way reveal the contents of this message to anyone. The integrity and security of this email cannot be guaranteed over the Internet. Therefore, the sender will not be held liable for any damage caused by the message.</div></td>
+              </tr>
+              </tbody>
+             </table>
+            </BODY></HTML>
+            
+            
+            ');
         }
 
         if ($this->email->send()) {
